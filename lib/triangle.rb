208b13031651e119473c0f13fcd80e80791601d0
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-20 18:15:22 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,15 @@
 class Triangle
-  # write code here
+
+  attr_reader :l1, :l2, :l3
+  def initialize(l1,l2,l3)
+    @l1 = l1
+    @l2 = l2
+    @l3 = l3
+  end
+
+  def kind
+    
+  class TriangleError < StandardError
+    # triangle error code
+  end
 end

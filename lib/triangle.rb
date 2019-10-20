c5ceba5e2a76ce7aa565fb8678a7942c8029d75d
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-20 18:50:46 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,14 +11,14 @@ class Triangle
   def kind
     triangle_sides = [@l1,@l2,@l3]
 
-    if triangle_sides.combination(2).all? {|a, b| a > b }
+    if !triangle_sides.combination(2).all? {|a, b| a > b }
       begin
         raise TriangleError
       rescue TriangleError => error
         puts error.message
       end
     elsif triangle_sides
-
+      
     end
   end
 
